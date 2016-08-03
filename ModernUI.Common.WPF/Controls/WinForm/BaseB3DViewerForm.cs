1324196644d8c498103bfae1f893ce68bab97f6b
Jason Kiscy <1424870395@qq.com>
2016-08-03 23:39:26 +0800

    框架搭建
    完成了wpf对winform的寄宿

---
+++ ModernUI.Common.WPF/Controls/WinForm/BaseB3DViewerForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ModernUI.Common.WPF.Controls.WinForm
+{
+    public partial class BaseB3DViewerForm : UserControl
+    {
+        public BaseB3DViewerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
